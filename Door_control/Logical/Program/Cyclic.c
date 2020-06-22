cae4aabbf435754bcea6e03b8e93d4a53507d808
thealexinator2904 <59167072+thealexinator2904@users.noreply.github.com>
2020-06-22 11:40:05 +0200

    Update Cyclic.c

--- Door_control/Logical/Program/Cyclic.c
+++ Door_control/Logical/Program/Cyclic.c
@@ -9,20 +9,19 @@
 
 struct motorDATA
 {
-	char M_rechts, M_links, stop_rechts, stop_links, lichtschranke;
+	char M_rechts, M_links, stop_rechts, stop_links, lichtschranke, anzeige;
 };
 
 enum
 {
-	STOP, OPENS, OPENED, CLOSES
+	INIT, STOP, OPENS, OPENED, CLOSES, ERROR
 };
 
-char handleDoor(char state, struct motorDATA *motor, char s_open, char isOTHERdoorClosed, char* isClosed);
+char handleDoor(char state, struct motorDATA *motor, char s_open, char isOTHERdoorClosed);
 
 void _CYCLIC ProgramCyclic(void)
 {
 	static struct motorDATA motorDATA_1={0,0,0,0,0}, motorDATA_2={0,0,0,0,0};
-	static char bull;
 	static int state1, state2;
 	
 	motorDATA_1.lichtschranke = B1;
@@ -33,45 +32,58 @@ void _CYCLIC ProgramCyclic(void)
 	motorDATA_2.stop_links = S7;
 	motorDATA_2.stop_rechts = S8;
 	
-	state1 = handleDoor(state1, &motorDATA_1, (S1 | S3), S7, &bull);
-	state2 = handleDoor(state2, &motorDATA_2, (S4 | S2), S5, &bull);
+	state1 = handleDoor(state1, &motorDATA_1, (S1 | S3), S7);
+	state2 = handleDoor(state2, &motorDATA_2, (S4 | S2), S5);
 
 	M1R = motorDATA_1.M_rechts;
 	M1L = motorDATA_1.M_links;
+	Anzeige_1 = motorDATA_1.anzeige;
 	
 	M2R = motorDATA_2.M_rechts;
 	M2L = motorDATA_2.M_links;
+	Anzeige_2 = motorDATA_2.anzeige;
 }
 
-char handleDoor(char state, struct motorDATA *motor, char s_open, char isOTHERdoorClosed, char* isClosed)
+char handleDoor(char state, struct motorDATA *motor, char s_open, char isOTHERdoorClosed)
 {
-	static TON_typ doorOpenTimer;
+	static TON_typ doorOpenTimer, blinkTimer, errorTimer;
 	doorOpenTimer.PT = 5000;
+	blinkTimer.PT = 200;
+	errorTimer.PT = 10000;
 	switch(state)
 	{
+		case INIT:
+			motor->anzeige = 0;
+			
+			if(!motor->stop_links)
+				state = CLOSES;
+			if(motor->stop_links)
+				state = STOP;
+			break;
+		
 		case STOP:
 			motor->M_rechts = 0;
 			motor->M_links = 0;
+			motor->anzeige = 0;
+			
 			if(s_open && isOTHERdoorClosed)
 				state = OPENS;
-			if(!motor->stop_links)
-				state = CLOSES;
 			break;
 		
 		case OPENS:
 			motor->M_rechts = 1;
 			motor->M_links = 0;
+			motor->anzeige = 0;
+			
 			if(motor->stop_rechts)
 				state = OPENED;
-			if(motor->lichtschranke)
-				state=STOP;
 			break;
 		
 		case OPENED:
 			motor->M_rechts = 0;
 			motor->M_links = 0;
+			motor->anzeige = 0;
 			doorOpenTimer.IN = 1;
-			
 
 			if(doorOpenTimer.Q)
 			{
@@ -83,12 +95,41 @@ char handleDoor(char state, struct motorDATA *motor, char s_open, char isOTHERdo
 		case CLOSES:
 			motor->M_rechts = 0;
 			motor->M_links = 1;
+			
+			blinkTimer.IN = 1;
+			if(blinkTimer.Q)
+			{
+				blinkTimer.IN = 0;
+				//Toggle anzeigeLED
+				motor->anzeige ^= 1;
+			}
+			
 			if(motor->stop_links)
 				state = STOP;
 			if(motor->lichtschranke)
-				state = STOP;
+				state = OPENS;
+			if(s_open)
+				state = OPENS;
+			
+			break;
+		
+		case ERROR:
+			motor->M_rechts = 0;
+			motor->M_links = 0;
+			motor->anzeige = 1;
+			
+			if(s_open && isOTHERdoorClosed)
+				state = OPENS;
 			break;
 	}
+	//errrorHandling
+	errorTimer.IN = motor->M_links || motor->M_rechts;
+	
+	if(errorTimer.Q)
+		state = ERROR;
+	
 	TON(&doorOpenTimer);
+	TON(&blinkTimer);
+	TON(&errorTimer)
 	return state;
 }
